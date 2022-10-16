fc813d6cf6d140dda5aae29cc94bce94dd6d0653
CrazyCoder83 <115811777+CrazyCoder83@users.noreply.github.com>
2022-10-16 22:06:54 +0530

    Update Final Program.py

--- Final Program.py
+++ Final Program.py
@@ -1,7 +1,7 @@
-n=int(input("Enter The Range Till Which To Find The Primr Factors Of : "))
+n=int(input("Enter The Range Till Which To Find The Prime Factors Of : "))
 for i in range(1,n+1):
     if (i==1):
-        print("1 = 1","9It's A Unique Number")
+        print("1 = 1","It's A Unique Number")
     else:
         c=0
         print(i,end=" = ")
@@ -21,7 +21,7 @@ for i in range(1,n+1):
             else:
                 j+=1
         if (c==1):
-            print("(It's A Prime Number")
+            print("(It's A Prime Number)")
         else:
             print()
             
